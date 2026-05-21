a1463303a46e131c6b4ae40f7cd3a217cb648137
李佳佳 <2593853553@qq.com>
2026-05-21 20:25:28 +0800

    feat: 初始化小程序

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <view>
+    <!-- 2.0.19支持autoBack，默认为false -->
+    <up-navbar title="个人中心" @rightClick="rightClick" :autoBack="true"> </up-navbar>
+  </view>
+</template>
+
+<script>
+// 定义方法
+const rightClick = () => {
+  console.log('rightClick')
+}
+
+const leftClick = () => {
+  console.log('leftClick')
+}
+</script>
+
+<style>
+page {
+  background-color: #fefcf8 !important;
+  box-sizing: border-box;
+}
+</style>
